ea965ffa5c22dc4d28402aed42a12f01644bb623
reebadiwan01 <reebadiwan01@gmail.com>
2023-05-08 08:54:59 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myresume';
+  name ="Reeba Diwan";
+  student ={
+    "rno":1,"name": "Sarah"
+  
+}
+arr=[1,5,6,8,4]
+stuArr =[
+  {
+    rno:1,name: abc
+  }
+]
+}
